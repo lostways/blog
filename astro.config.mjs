58185c64252a4e71336bbe149fb68189f5f88344
lostways <andrew@lostways.com>
2023-09-22 06:42:14 -0700

    domain is on root

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  site: 'https://blog.lostways.com',
+  site: 'https://lostways.com',
 });
